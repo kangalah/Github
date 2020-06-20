015cc30b997c19b8df3a45524b6a0dd7ace56ee1
kangalah <nyongesajoannanjala@gmail.com>
2020-06-20 13:03:27 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { FormComponent } from './form/form.component';
+import { UsersComponent } from './users/users.component';
+import { UpperCasePipe } from './upper-case.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RepositoriesComponent,
+    FormComponent,
+    UsersComponent,
+    UpperCasePipe
   ],
   imports: [
     BrowserModule,
