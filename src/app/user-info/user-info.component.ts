b7514c6879b06625a01c8e2fccbd0da8168876b8
kangalah <nyongesajoannanjala@gmail.com>
2020-06-21 10:04:08 +0300

    add a finduser function

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchRequestService } from '../search-request.service';
+import { Repository } from '../repository';
+import { Users } from '../users';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
+  providers: [SearchRequestService],
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  public searchme = 'kangalah';
+  public githubUser: string;
 
-  ngOnInit(): void {
+  users: Users;
+  repository: Repository;
+
+  findUser(username) {
+    this.githubUser = '';
+    this.searchme = username;
+    this.ngOnInit();
+  }
+
+  constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+  ngOnInit() {
+    this.githubUserRequest.githubUser(this.searchme);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchme);
+    console.log(this.userRepos);
   }
 
 }
