0c85ef5149790d45570a25089641a01443f3db0a
kangalah <nyongesajoannanjala@gmail.com>
2020-06-22 05:26:39 +0300

    add constructor with the searchRequestFunction for the userrepos

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Search } from '../search';
+import { SearchRequestService } from '../search-request.service';
+import { Repository } from '../repository';
+import { Users } from '../users';
+
 
 @Component({
   selector: 'app-form',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  searchInfo = new Search ('');
+  @Output() getName = new EventEmitter<Search>();
 
-  constructor() { }
+  searchFor(data){
+    this.getName.emit(data.value.find);
+    console.log(data.value.find)
+    data.reset();
+  }
 
-  ngOnInit(): void {
+  public searchMe = '';
+  public githubUser: string;
+
+  users: Users;
+  repository:Repository;
+  public searchRepo:string;
+
+  findUser(Username){
+    this.githubUser = '';
+    this.searchMe = Username;
+    this.ngOnInit();
+  }
+
+  constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+  ngOnInit(){
+    this.githubUserRequest.githubUser(this.searchMe);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchMe);
+    console.log(this.userRepos);
+  }
+
+  searchRepos(){
+    this.searchRepo = '';
   }
 
 }
